96566d006aba42064c18019438160dfbbb51e2d0
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2019-01-06 16:47:56 +0100

    init repo, create server and basic shema types

---
+++ server/schema.js
@@ -0,0 +1,81 @@
+const graphql = require('graphql');
+const _ = require('lodash')
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
+} = graphql;
+
+const books = [
+    { title: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+    { title: 'The Final Empire', genre: 'Fantasy', id: '2' },
+    { title: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString},
+        title: { type: GraphQLString},
+        author: { type: GraphQLString},
+        genre: { type: GraphQLString},
+        description: { type: GraphQLString},
+
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLString},
+        name: { type: GraphQLString},
+        surname: { type: GraphQLString},
+        books: { type: GraphQLList(BookType)},
+
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {
+                id: {type: GraphQLString}
+            },
+            resolve(parent, args) {
+               return _.find(books, {id: args.id})
+            }
+        },
+        books: {
+            type: GraphQLList(BookType),
+            resolve() {
+                return books
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: {
+                id: {type: GraphQLString}
+            },
+            resolve(parent, args) {
+                //get data from db/other source
+            }
+        },
+        authors: {
+            type: GraphQLList(AuthorType),
+            resolve(parent, args) {
+                //get data from db/other source
+            }
+        },
+
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
